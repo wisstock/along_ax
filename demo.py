3cf16e2fed2d7ae44e22dfdef4f565063babc4e6
wisstock <omnia.fatum@gmail.com>
2020-10-23 13:20:03 +0300

    skeleton demo

--- demo.py
+++ demo.py
@@ -20,6 +20,7 @@ import matplotlib.patches as patches
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from skimage import filters
+from skimage.morphology import medial_axis, skeletonize
 from skimage.external import tifffile
 
 sys.path.append('modules')
@@ -35,52 +36,67 @@ plt.rcParams['figure.facecolor'] = '#272b30'
 plt.rcParams['image.cmap'] = 'inferno'
 
 
-img_series = tifffile.imread(os.path.join(sys.path[0], 'data/two/hpca_cfp.tif'))
+img_series = tifffile.imread(os.path.join(sys.path[0], 'data/hpca_cfp.tif'))
 img_series = d.backRm(img_series)
+img_series = img_series[:,:,:550]
 
 
-mask = d.hystMask(img_series[0])
+mask = d.hystMask(img_series[0], low=0.2)
+# thresh = filters.threshold_otsu(img_series[0])
+# mask = img_series[0] > thresh
+
+# skel, dist = medial_axis(mask, return_distance=True, )
+# dist_plot = dist * skel
+skel = skeletonize(mask, method='lee')
+
 diff_series = d.sDerivate(img_series, mask,
 	                      sigma=4, mean_win=2, mean_space=0, mode='binn')
 
 
-num = 6
-
-ax2 = plt.subplot(121)
-img2 = ax2.imshow(img_series[num])
-dvdr2 = make_axes_locatable(ax2)
-cax2 = dvdr2.append_axes('right', size='2%', pad=0.05)
-plt.colorbar(img2, cax=cax2)
-
-ax3 = plt.subplot(122)
-img3 = ax3.imshow(diff_series[num],
-	       vmin=-1, vmax=1,
-	       cmap='bwr')
-dvdr3 = make_axes_locatable(ax3)
-cax3 = dvdr3.append_axes('right', size='2%', pad=0.05)
-plt.colorbar(img3, cax=cax3)
-
-plt.show()
 
 
+# num = 6
 
-# # save series
-# a = 1
-# for frame in diff_series:
-# 	plt.figure()
-# 	ax = plt.subplot()
-# 	img = ax.imshow(frame, cmap='bwr')
+# ax2 = plt.subplot(121)
+# img2 = ax2.imshow(img_series[num])
+# dvdr2 = make_axes_locatable(ax2)
+# cax2 = dvdr2.append_axes('right', size='2%', pad=0.05)
+# plt.colorbar(img2, cax=cax2)
 
-# 	ax.text(20,20,a,fontsize=18)
-# 	# rect = patches.Rectangle((0,0),100,100,linewidth=1,edgecolor='w',facecolor='k')
-# 	# ax.add_patch(rect)
+
+# ax1 = plt.subplot(122)
+# ax1.imshow(skel)
+# ax1.contour(mask, size=0.5, colors='b')
+
+# ax3 = plt.subplot(122)
+# img3 = ax3.imshow(diff_series[num],
+# 	       vmin=-1, vmax=1,
+# 	       cmap='bwr')
+# dvdr3 = make_axes_locatable(ax3)
+# cax3 = dvdr3.append_axes('right', size='2%', pad=0.05)
+# plt.colorbar(img3, cax=cax3)
+
+# plt.show()
+
+
+
+# save series
+a = 1
+for frame in diff_series:
+	plt.figure()
+	ax = plt.subplot()
+	img = ax.imshow(frame, cmap='bwr')
+
+	ax.text(20,20,a,fontsize=18)
+	# rect = patches.Rectangle((0,0),100,100,linewidth=1,edgecolor='w',facecolor='k')
+	# ax.add_patch(rect)
 	
 
-# 	ax.axis('off')
-# 	plt.savefig('frame_{}.png'.format(a))
+	ax.axis('off')
+	plt.savefig('frame_{}.png'.format(a))
 
-# 	logging.info('Frame {} saved!'.format(a))
-# 	a += 1
+	logging.info('Frame {} saved!'.format(a))
+	a += 1
 
 
 # # animation
